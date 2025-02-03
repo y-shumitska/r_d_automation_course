39c94df79befd607170f293c295143ffd6c785ea
Yevheniia Tymchenko <shumitska@gmail.com>
2025-02-03 22:12:53 +0200

    Adding the homework 3

---
+++ homework_3/src/arifmetics.js
@@ -0,0 +1,29 @@
+const studentName = 'Yevheniia';
+const studentSurname = 'Tymchenko';
+const birthYear = 1995;
+let currentYear = 2025;
+let isManualQA = true;
+let isAQA = false;
+let currentProject = null;
+let yearsInAutomation;
+
+const fulllName = studentName +' '+ studentSurname;
+console.log(fulllName);
+
+let age = currentYear - birthYear;
+console.log(age);
+
+age++;
+let ageNextYear = age;
+console.log(ageNextYear);
+
+let internetNickname = studentName + birthYear;
+console.log(internetNickname);
+
+console.log(isManualQA);
+
+console.log(isAQA);
+
+console.log(currentProject);
+
+console.log(yearsInAutomation);
