3a23a7c1a84622c6365a5361a8bd19d5d94933a8
Yevheniia Tymchenko <shumitska@gmail.com>
2025-02-19 15:49:11 +0200

    Corrected homework 7

--- homework_7/src/arrow-functions.ts
+++ homework_7/src/arrow-functions.ts
@@ -2,26 +2,21 @@ const numbers = [4, 0.1, 694, 1233, 54];
 const stringValues = ['Hello World!', 'Jane', 'is', 'becoming', 'AQA'];
 
 const calculateArraySum = (someArray: string[] | number []): number | string => {
-    let sum: number | string = 0;
-    someArray.forEach((element: string | number) => {
-        if (!isNaN(element as number)) {
-            sum = 0;
-        } else {
-            sum = '';
-        };
-    });
-    someArray.forEach((element: string | number) => {
-        if (typeof sum === 'string') {
-            sum = sum as string;
-            element = element as string;
-            sum = sum + element;
-        } else {
-            sum = sum as number;
-            element = element as number;
-            sum = sum + element;
-        }
-    });
-    return sum;
+    if (someArray.length > 0 && typeof someArray[0] === 'string') {
+        let sum = '';
+        (someArray as string[]).forEach((element) => {
+            sum += element;
+        });
+        return sum;
+    } else if (someArray.length > 0) {
+        let sum = 0;
+        (someArray as number[]).forEach((element) => {
+            sum += element;
+        });
+        return sum;
+    } else {
+        return '';
+    }
 };
 
 console.log('The sum of the number elements of the array: ' + calculateArraySum(numbers));
